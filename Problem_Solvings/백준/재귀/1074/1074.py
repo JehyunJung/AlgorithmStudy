6bab91ad7eec6f36026213a38bc6e5f2cbd37748
jehyunJung <toojey7@gmail.com>
2020-03-24 23:59:07 +0900

    study: algorithms(based on recursion)_1 #200324

---
+++ Problem_Solvings/백준/재귀/1074/1074.py
@@ -0,0 +1,33 @@
+def solution(space,xpos,ypos):
+    space_count=2**space
+    space_count*=space_count
+
+    current_xside=(0,2**space)
+    current_yside=(0,2**space)
+    count=0
+    while True:
+        med_y=(current_yside[0]+current_yside[1])//2
+        med_x=(current_xside[0]+current_xside[1])//2
+
+        if ypos >= med_y and ypos <current_yside[1]:
+            count+=space_count//2
+            current_yside=(med_y,current_yside[1])
+        else:
+            current_yside=(current_yside[0],med_y)
+
+        if xpos > med_x and xpos <=current_xside[1]:
+            count+=space_count//4
+            current_xside=(med_x,current_xside[1])
+        else:
+            current_xside=(current_xside[0],med_x)
+
+        if current_yside[0]==ypos and current_xside[1]==xpos:
+            count+=1
+            break
+        space_count//=4
+
+    print(count)
+
+if __name__ == "__main__":
+    space,y_pos,x_pos=list(map(int, input().split(' ')))
+    solution(space,x_pos,y_pos)
---
+++ Problem_Solvings/백준/재귀/2747/2747.py
@@ -0,0 +1,18 @@
+import time
+def fibo_1(num):
+    if num<=0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibo_1(num-1)+fibo_1(num-2)
+def fibo_2(num):
+    fibo_data=[0,1]
+    for index in range(2,num+1):
+        fibo_data.append(fibo_data[index-1]+fibo_data[index-2])
+    return fibo_data[num]
+
+if __name__ =="__main__":
+    start=time.time()
+    print(fibo_2(int(input())))
+    print(time.time()-start," secs passed")
